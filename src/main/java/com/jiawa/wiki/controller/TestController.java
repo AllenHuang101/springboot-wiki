cfa99a4adefa6f6a7ead37140c299ccd9d01b87f
allen3_huang <allen3_huang@asus.com>
2021-06-27 18:32:04 +0800

    2-7 增加hello街口測試test.http

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,8 +1,7 @@
 package com.jiawa.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,6 +10,12 @@ public class TestController {
     @GetMapping("/hello")
     //@RequestMapping(value="/hello", method = RequestMethod.GET)
     public String hello() {
-        return "Hello World!";
+        return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    //@RequestMapping(value="/hello", method = RequestMethod.GET)
+    public String hello(String name) {
+        return "Hello World! Post, " + name;
     }
 }
