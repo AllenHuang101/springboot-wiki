7164e53ee6e518d7bdb250c1f73a789f09188819
allen3_huang <allen3_huang@asus.com>
2021-07-12 21:25:29 +0800

    4-7 製作 the-header 組件

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,18 +1,6 @@
 <template>
  <a-layout>
-     <a-layout-header class="header">
-       <div class="logo" />
-       <a-menu
-         theme="dark"
-         mode="horizontal"
-         v-model:selectedKeys="selectedKeys1"
-         :style="{ lineHeight: '64px' }"
-       >
-         <a-menu-item key="1">nav 1</a-menu-item>
-         <a-menu-item key="2">nav 2</a-menu-item>
-         <a-menu-item key="3">nav 3</a-menu-item>
-       </a-menu>
-     </a-layout-header>
+     <the-header></the-header>
      <router-view/>
      <a-layout-footer style="text-align: center">
        Allen 電子書
@@ -21,23 +9,14 @@
 </template>
 
 <script lang="ts">
-import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons-vue';
-import { defineComponent, ref } from 'vue';
-export default defineComponent({
-  components: {
-    UserOutlined,
-    LaptopOutlined,
-    NotificationOutlined,
-  },
-  setup() {
-    return {
-      selectedKeys1: ref<string[]>(['2']),
-      selectedKeys2: ref<string[]>(['1']),
-      collapsed: ref<boolean>(false),
-      openKeys: ref<string[]>(['sub1']),
-    };
-  },
-});
+  import TheHeader from '@/components/the-header.vue'
+  import { defineComponent, ref } from 'vue';
+  export default defineComponent({
+    name: 'app',
+    components: {
+      TheHeader,
+    },
+  });
 </script>
 
 <style>
